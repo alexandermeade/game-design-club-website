2a9aafabb1b3ef9a23cd99ea31236b43c35ba04f
alexandermeade <thisguys348@gmail.com>
2026-01-21 22:33:33 -0500

    Init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import logo from '$lib/assets/msu(1).png';
+</script>
+
+
+<center>
+<h1>Welcome to MSU game design club</h1>
+<p>This is super ugly but not for long</p>
+
+<img src={logo} alt="Description of the image" width="300" height="200">
+</center>
